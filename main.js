11ab691740e7aa77e4911a6c3133fdb9beb34a6c
BruceChen034020 <kchen0919@gmail.com>
2021-09-09 22:57:14 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+/*
+感謝您觀看這份程式碼
+作品名稱: N/A
+作者: N/A
+聯絡方式
+  Facebook連結: https://www.facebook.com/bruce.chen.372
+  LINE ID: brucechen0
+最後修改日期: N/A
+版本: N/A
+發表於: https://brucechen034020.github.io/
+程式碼尺度
+  N/A
+作者註解:
+  1. 如本網頁有 bug 請用 Facebook(Messenger) 通知 Bruce Chen，謝謝。
+  2. 如有任何建議，請用 Facebook(Messenger) 通知 Bruce Chen，謝謝。
+*/
+
+/* Global variables */
+var img1; // (Image file)
+
+/* p5 functions */
+function setup(){
+  img1 = loadImage("20210909.PNG");
+  console.log(typeof img1)
+  image(0, 0, 500, 500)
+}
+
+function draw(){
+  if (mouseIsPressed === true)
+    {
+      line(mouseX, mouseY, pmouseX, pmouseY);
+    }
+}
